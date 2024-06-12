aff3a76cf8d603359cca581b7ed852b408100cf6
jdavida56 <jdavida56@hotmail.com>
2024-06-11 21:42:39 -0500

    primerojava

---
+++ proyectoJuan/proyectoJuanJava/src/proyectoJuanJava/proyectoJuan.java
@@ -0,0 +1,12 @@
+package proyectoJuanJava;
+
+public class proyectoJuan{
+    public static void main(String[] args) {
+        System.out.println("hola mundo estoy triunfando");
+        
+    }
+}
+
+
+    
+
